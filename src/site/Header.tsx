729e92982807503770f3ea360d78478819ae44d4
Ledol <dzmitryliavonenka@gmail.com>
2022-07-02 10:59:48 +0300

    Added new components Header, Body, Footer

---
+++ src/site/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+type HeaderType = {
+    title: string
+}
+
+export const Header = (props:HeaderType) => {
+    return (
+        <div>
+            {props.title}
+        </div>
+    );
+};
+
